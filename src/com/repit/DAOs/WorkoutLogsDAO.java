f66cb51609a1990f59362b88182d172b83702f46
Myrhen <nmyrhens714@gmail.com>
2026-03-31 12:18:24 -0700

    db, jdbc, added files

---
+++ src/com/repit/DAOs/WorkoutLogsDAO.java
@@ -0,0 +1,10 @@
+package com.repit.DAOs;
+import com.repit.Model.WorkoutLog;
+
+public class WorkoutLogsDAO {
+    //public list<WorkoutLog> getLogsByUserId(int userId){
+
+   // }
+
+
+}
---
+++ src/com/repit/Model/WorkoutLog.java
@@ -0,0 +1,7 @@
+package com.repit.Model;
+
+public class WorkoutLog {
+    private int logId;
+    private int userId;    // <-- links this log to a user
+    private String desc;
+}
