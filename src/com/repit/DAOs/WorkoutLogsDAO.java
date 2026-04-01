b04f596ac84472ce90627b5dc9c2ff6e4e74b54e
Myrhen <nmyrhens714@gmail.com>
2026-04-01 14:43:52 -0700

    WorkoutLogsDAO, WorkoutLog

--- src/com/repit/DAOs/WorkoutLogsDAO.java
+++ src/com/repit/DAOs/WorkoutLogsDAO.java
@@ -1,10 +1,95 @@
 package com.repit.DAOs;
 import com.repit.Model.WorkoutLog;
 
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.util.ArrayList;
+
 public class WorkoutLogsDAO {
-    //public list<WorkoutLog> getLogsByUserId(int userId){
+    private int userId;
 
-   // }
+    private static final String TABLE_SQL =
+            "CREATE TABLE IF NOT EXISTS workout_logs (" +
+            "logId INTEGER PRIMARY KEY," +
+            "userId TEXT NOT NULL," +
+            "desc TEXT NOT NULL," +
+            "isCompleted INTEGER NOT NULL"+
+            ")";
 
+    private static final String INSERT_SQL =
+            "INSERT INTO workout_logs (userId, desc, isCompleted) " +
+                    "VALUES (?,?,?)";
 
+    private static final String SELECT_SQL =
+            "SELECT * FROM workout_logs WHERE userId = ?";
+
+    private static final String DELETE_SQL =
+            "DELETE FROM workout_logs WHERE logId = ? AND userID = ?";
+
+    WorkoutLogsDAO(int newId){
+        userId = newId;
+    }
+
+    public ArrayList<WorkoutLog> getLogs(){
+        ArrayList<WorkoutLog> listLogs = new ArrayList<>();
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:repit.db");
+            PreparedStatement pstmt = conn.prepareStatement(SELECT_SQL);
+            pstmt.setInt(1, userId);
+            pstmt.executeUpdate();
+
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()){
+                WorkoutLog newLog = new WorkoutLog(
+                        rs.getInt("logId"),
+                        rs.getInt("userId"),
+                        rs.getString("desc"),
+                        rs.getInt("isCompleted") == 1
+                );
+                listLogs.add(newLog);
+            }
+            return listLogs;
+        } catch (Exception e){
+            System.out.println("Database Error: "+ e.getMessage());
+            return null;
+        }
+    }
+
+    public boolean saveWorkoutLog(WorkoutLog logTosave){
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:repit.db");
+            Statement stmt = conn.createStatement();
+            stmt.execute(TABLE_SQL);
+
+            PreparedStatement pstmt = conn.prepareStatement(INSERT_SQL);
+            pstmt.setInt(1, logTosave.getUserId());
+            pstmt.setString(2, logTosave.getDesc());
+            pstmt.setInt(3, logTosave.getCompletion());
+            pstmt.executeUpdate();
+            return true;
+
+        } catch (Exception e){
+            System.out.println("Database Error: "+ e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean deleteWorkoutLog(int logId){
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:repit.db");
+            PreparedStatement pstmt = conn.prepareStatement(DELETE_SQL);
+            pstmt.setInt(1, userId);
+            pstmt.setInt(2, logId);
+            pstmt.executeUpdate();
+            return true;
+        } catch (Exception e){
+            System.out.println("Database Error: "+ e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean deleteWorkoutLog(WorkoutLog log){
+        int logId = log.getLogId();
+        return deleteWorkoutLog(logId);
+    }
 }
--- src/com/repit/Model/WorkoutLog.java
+++ src/com/repit/Model/WorkoutLog.java
@@ -4,4 +4,17 @@ public class WorkoutLog {
     private int logId;
     private int userId;    // <-- links this log to a user
     private String desc;
+    private boolean isCompleted = false;
+
+    public int getLogId(){ return logId; }
+    public int getUserId(){ return userId;}
+    public String getDesc() { return desc; }
+    public int getCompletion() { return isCompleted ? 1 : 0; }
+
+    public WorkoutLog(int pLogId, int pUserId, String pDesc, boolean pIsCompleted){
+        logId = pLogId;
+        userId = pUserId;
+        desc = pDesc;
+        isCompleted = pIsCompleted;
+    }
 }
