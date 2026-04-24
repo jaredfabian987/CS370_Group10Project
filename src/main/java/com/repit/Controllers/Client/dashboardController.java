f5bdee5ae3736ab3404abe41407aadf8ed859685
kpgalarno <philias1116@gmail.com>
2026-04-23 18:01:40 -0700

    started dashboardController, started linking fx:ids with updater functions. Started viewFactory (still not finished).

--- src/main/java/com/repit/Controllers/Client/dashboardController.java
+++ src/main/java/com/repit/Controllers/Client/dashboardController.java
@@ -1,13 +1,18 @@
 package com.repit.Controllers.Client;
 
+import com.repit.Model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.text.Text;
 
-public class dashboardController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class dashboardController implements Initializable {
 
     //displays estimated time to complete the workout for the day
     @FXML
@@ -37,16 +42,65 @@ public class dashboardController {
     @FXML
     private ProgressBar workoutCompletionProgressBar;
 
+    //User object
+    private User loggedUser;
+
+    //Dashboard Service Object
+    //private DashboardService dashboardService;
+
+    //Initialization methods:
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setStateLoading();
+    }
+
+    public void setLoggedUser(User loggedUser) {
+        this.loggedUser = loggedUser;
+        setStateLoading();
+    }
+
+    //States:
+    //Loading State, inserts placeholder text while service retrieves database items
+    public void setStateLoading() {
+        todaysWorkoutLabel.setText("Loading...");
+        estimatedTimeLabel.setText("Estimated Time: Loading..");
+        workoutCompletionLabel.setText("0 / 0 workouts");
+        workoutCompletionProgressBar.setProgress(0);
+        startWorkoutButton.setDisable(true);
+        openPlannerButton.setDisable(true);
+    }
+
+    //Loaded State, inserts the data returned by the service dispatcher
+    private void loadDashboardData() {
+        //if no user is logged in, return
+        if (loggedUser != null) {
+            return;
+        }
+
+        //Insert service function to get data
+        //
+
+        //Insert
+        //
+    }
+
+
+
+
+    //Buttons and Events:
+    //Logs out loads, login fxml fil
     @FXML
     void logoutClicked(ActionEvent event) {
 
     }
 
+    //opens Planner, loads fxml file
     @FXML
     void openPlannerClicked(ActionEvent event) {
 
     }
 
+    //Starts workout, loads fxml file
     @FXML
     void startWorkoutClicked(ActionEvent event) {
 
--- src/main/java/com/repit/View/viewFactory.java
+++ src/main/java/com/repit/View/viewFactory.java
@@ -1,4 +1,41 @@
 package com.repit.View;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class viewFactory {
+    //Class variables:
+    //stage is passed in from main
+    private final Stage stage;
+
+    //Constructor:
+    //Passes in stage from main and intializes "stage" variable
+    public viewFactory(Stage stage){
+        this.stage = stage;
+    }
+
+    //Methods:
+    //Take in String fxml and return
+    public <T> T switchScene(String fxml) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+            Parent root = loader.load();
+
+
+
+            return loader.getController();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load scene",e);
+        }
+    }
+
+    //Pass in String fxml and return absolute name of fxml
+    public URL getResource(String fxml) {
+        URL resource = viewFactory.class.getResource("/" + fxml);
+        return resource;
+    }
 }
